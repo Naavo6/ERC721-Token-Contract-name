7d77968f9ca7743a0eadf605fe49877e74f9b65a
Naavo6 <jayekhandeh4@gmail.com>
2023-12-24 23:04:49 +0330

    Add functions to the main contract before the _update function

--- contracts/ERC721TokenContractName.sol
+++ contracts/ERC721TokenContractName.sol
@@ -6,14 +6,15 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import {IERC721Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+import {IERC721TCNReceiver} from "contracts/IERc721TokenContractNameReceiver.sol";
 
 
 
 
-contract ERC721TokenContractName is Context, IERC721Errors {
+contract ERC721TokenContractName is Context, IERC721Errors, IERC721TCNReceiver {
     event Approval(address indexed owner, address indexed approved, uint16 indexed tokenId);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Transfer(address indexed from, address indexed to, uint16 indexed tokenId);
 
 
     using Address for address;
@@ -25,16 +26,19 @@ contract ERC721TokenContractName is Context, IERC721Errors {
 
     bytes10 private _symbol;
 
-    struct TokInfo {
+    struct MintInfo {
         uint16 maxMinted;
         uint16 currentTokens;
     }
 
-    TokInfo private tokInfo;
+
+
+   MintInfo private mintInfo;
 
 
     address[1201] private _owners;
 
+    bool[1201] private ban;
 
     mapping(address owner => uint16[1201]) private _balanceAndTokId;
 
@@ -104,10 +108,35 @@ contract ERC721TokenContractName is Context, IERC721Errors {
         return _operatorApprovals[owner][operator];
     }
 
+    function transferFrom(address from, address to, uint16 tokenId) public virtual {
+        if (to == address(0)) {
+            revert ERC721InvalidReceiver(address(0));
+        }
+
+        // Setting an "auth" arguments enables the `_isAuthorized` check which verifies that the token exists
+        // (from != 0). Therefore, it is not needed to verify that the return value is not 0 here.
+        address previousOwner = _update(to, tokenId, _msgSender());
+        if (previousOwner != from) {
+            revert ERC721IncorrectOwner(from, tokenId, previousOwner);
+        }
+    }
+
+    function safeTransferFrom(address from, address to, uint16 tokenId) public {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(address from, address to, uint16 tokenId, bytes memory data) public virtual {
+        transferFrom(from, to, tokenId);
+        _checkOnERC721Received(from, to, tokenId, data);
+    }
 
 
+    function _update(address to, uint16 tokenId, address auth) private returns (address) {
 
-    function _aprove(address to, uint16 tokenId, address auth) internal {
+    }
+
+
+    function _aprove(address to, uint16 tokenId, address auth) private {
         address owner = _requireOwned(tokenId);
         require(owner != to, "The address of the owner and the operator must be different");
        if (owner != auth && !isApprovedForAll(owner, auth)){
@@ -121,15 +150,41 @@ contract ERC721TokenContractName is Context, IERC721Errors {
     
 
 
-    function _requireOwned(uint16 tokenId) internal view returns (address) {
-        require(0 < tokenId && tokenId <= tokInfo.currentTokens, "The ID entered is invalid. It must be in this interval 0 < id <= current tokens"); 
+    function _requireOwned(uint16 tokenId) private view returns (address) {
+        require(0 < tokenId && tokenId <= mintInfo.currentTokens, "The ID entered is invalid. It must be in this interval 0 < id <= current tokens"); 
         return _owners[tokenId];
     }
 
 
-    function _baseURI() internal pure returns (string memory) {
+    function _baseURI() private pure returns (string memory) {
         return "";
     }
+
+    function _checkOnERC721Received(address from, address to, uint16 tokenId, bytes memory data) private {
+        if (to.code.length > 0) {
+            try IERC721TCNReceiver(to).onERC721Received(_msgSender(), from, tokenId, data) returns (bytes4 retval) {
+                if (retval != IERC721TCNReceiver.onERC721Received.selector) {
+                    revert ERC721InvalidReceiver(to);
+                }
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert ERC721InvalidReceiver(to);
+                } else {
+                    /// @solidity memory-safe-assembly
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        }
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint16 tokenId,
+        bytes calldata data
+    ) external returns (bytes4){}
 
 
     
