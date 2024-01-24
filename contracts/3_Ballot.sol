aba1486208ab9e523b072ad27164f54aca5de572
Naavo6 <jayekhandeh4@gmail.com>
2024-01-24 23:12:43 +0330

    BallotTCN

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -8,21 +8,30 @@ pragma solidity ^0.8.20;
  */
 contract BallotTCN {
 
+    event etaSecondsIsActivated(address indexed _this, uint48 _etaSeconds);
+    event TheVoteWasRegistered(address indexed sender, address indexed _this);
+
     error invalidProposer(address proposer);
+    error VotingHasNotStarted(uint48 _voteStart);
+    error VotingIsOver(uint256 _deadLine);
 
     struct Voter {
-        uint weight; // weight is accumulated by delegation
+        uint16 weight; // weight is accumulated by delegation
+        uint16 delegateWeight;
         bool voted;  // if true, that person already voted
         address delegate; // person delegated to
-        uint vote;   // index of the voted proposal
+        uint256 votedTime;   // index of the voted proposal
     }
 
-    struct Proposal {
+    struct BALLOtPARAM {
         // If you can limit the length to a certain number of bytes, 
         // always use one of bytes1 to bytes32 because they are much cheaper
         bytes32 name;   // short name (up to 32 bytes)
-        uint voteCount; // number of accumulated votes
+        uint16 voteCount; // number of accumulated votes
+        uint16 observed;
     }
+
+    BALLOtPARAM public _ballotParam;
 
     struct VOTINGPARAM {
         uint16 quorum;
@@ -32,6 +41,7 @@ contract BallotTCN {
     }
 
     VOTINGPARAM private _votingParams;
+    uint256 private _deadLine;
 
     struct CALLDATAPARAM {
         string signature;
@@ -56,26 +66,24 @@ contract BallotTCN {
 
     PROPOSALPARAM private _proposalParams;
 
-    address public chairperson;
-
     mapping(address proposerAdd => address targetAdd) private _targetConnectors;
     mapping(address => Voter) public voters;
 
-    Proposal[] public proposals;
+
 
     // /** 
     //  * @dev Create a new ballot to choose one of 'proposalNames'.
     //  * @param proposalNames names of proposals
     //  */
     constructor(
-        CALLDATAPARAM memory callDataParam_, string memory description_,
+        CALLDATAPARAM memory callDataParam_,
         uint256 value_,
-        VOTINGPARAM memory votingParams_) { //bytes32[] memory proposalNames
+        VOTINGPARAM memory votingParams_,
+        bytes32 name_) {
 
         // calldata param
-        _callDataParam.signature = callDataParam_.signature;
-        _callDataParam.descriptionHash = keccak256(abi.encode(description_));
-        _callDataParam.governance = callDataParam_.governance;
+        _callDataParam = callDataParam_;
+        _callDataParam.descriptionHash = keccak256(abi.encode(_callDataParam.description));
 
         // proposal param
         _proposalParams.proposer = msg.sender;
@@ -86,23 +94,13 @@ contract BallotTCN {
         _proposalParams.value = value_;
 
         // voting param
-        _votingParams.quorum = votingParams_.quorum;
-        _votingParams.voteDuration = votingParams_.voteDuration;
-        _votingParams.voteStart = votingParams_.voteStart;
-        _votingParams.etaSeconds = votingParams_.etaSeconds;
+        _votingParams = votingParams_;
+        _deadLine = _votingParams.voteStart + _votingParams.voteDuration;
 
-        // chairperson = msg.sender;
-        // voters[chairperson].weight = 1;
-
-        // for (uint i = 0; i < proposalNames.length; i++) {
-        //     // 'Proposal({...})' creates a temporary
-        //     // Proposal object and 'proposals.push(...)'
-        //     // appends it to the end of 'proposals'.
-        //     proposals.push(Proposal({
-        //         name: proposalNames[i],
-        //         voteCount: 0
-        //     }));
-        // }
+        // Ballot param
+        _ballotParam.name = name_;
+        _ballotParam.voteCount = 0;
+        _ballotParam.observed = 0;
     }
 
 
@@ -126,41 +124,22 @@ contract BallotTCN {
         proposalId = uint256(keccak256(abi.encode(_proposalParams.target, _proposalParams.value, getBallotCallData(), _callDataParam.descriptionHash)));
     }
 
+    function getBallotParam() public view returns (bytes32 name, uint16 voteCount, uint16 observed) {
+        return (_ballotParam.name, _ballotParam.voteCount, _ballotParam.observed);
+    }
 
-
-    // function setBallotForupdateMintInfo(
-    //     uint16 newmaxMint,
-    //     uint256 newregistrationStartTime,
-    //     address newexecutor,
-    //     address newbankAddress,
-    //     uint256 newmintPrice,
-    //     uint16[1201] memory newTokenId,
-    //     bytes32 descriptionHash,
-    //     address governance,
-    //     uint256 value,
-    //     address proposer) public {
-    //     //     require(_msgSender() == mintInfo.executor, "You do not have access to this function");
-    //     //    if (1000 < newmaxMint && newmaxMint <= 1200) { 
-    //     //         if (newmaxMint > mintInfo.nRegistrants && newregistrationStartTime >= block.timestamp) {
-    //     //             revert ERC721ParamArentAcceptable(newmaxMint, newregistrationStartTime);
-    //     //         } else if ((newmaxMint - mintInfo.currentTokens) != newTokenId[0]) {
-    //     //             revert Erc721InvalidTotalNewTokenId(newTokenId[0]);
-    //     //         } else if (governance != _governance) {
-    //     //             revert ERC721InvalidGovernanceAddress(governance);
-    //     //         }
-    //         bytes memory callData = abi.encodeWithSignature("updateMintInfo(uint16,uint256,address,address,uint256,uint16[],bytes32,address)", newmaxMint, newregistrationStartTime, newexecutor, newbankAddress, newmintPrice, newTokenId, descriptionHash, governance);
-    //         //(bool suc,) = governance.call(abi.encodeWithSignature("propose(address,uint256,bytes,bytes32)", address(this), value, callData, descriptionHash, proposer, ballotAddress));
-    //         // } else revert ERC721CantMoreThan1200(newmaxMint);
-    //     }
 
     /** 
      * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
      * @param voter address of voter
      */
     function giveRightToVote(address voter) public {
+        if (!isVotingDeadLine()) {
+            revert VotingIsOver(_deadLine);
+        }
         require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
+            msg.sender == _proposalParams.proposer,
+            "Only proposer can give right to vote."
         );
         require(
             !voters[voter].voted,
@@ -175,6 +154,9 @@ contract BallotTCN {
      * @param to address to which vote is delegated
      */
     function delegate(address to) public {
+        if (!isVotingDeadLine()) {
+            revert VotingIsOver(_deadLine);
+        }
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
         require(to != msg.sender, "Self-delegation is disallowed.");
@@ -186,34 +168,42 @@ contract BallotTCN {
             require(to != msg.sender, "Found loop in delegation.");
         }
         sender.voted = true;
+        _ballotParam.observed += sender.weight;
         sender.delegate = to;
         Voter storage delegate_ = voters[to];
         if (delegate_.voted) {
             // If the delegate already voted,
             // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
+            _ballotParam.voteCount += (sender.weight + sender.delegateWeight);
         } else {
             // If the delegate did not vote yet,
             // add to her weight.
-            delegate_.weight += sender.weight;
+            delegate_.delegateWeight += (sender.weight + sender.delegateWeight);
         }
+
     }
 
     /**
-     * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
-     * @param proposal index of proposal in the proposals array
+     * @dev Give your vote (including votes delegated to you).
+     * @param opinion index of opinion in the vote function
      */
-    function vote(uint proposal) public {
+    function vote(bool opinion) public {
+        if (block.timestamp < _votingParams.voteStart) { 
+            revert VotingHasNotStarted(_votingParams.voteStart);
+        } else if (!isVotingDeadLine()) {
+            revert VotingIsOver(_deadLine);
+        }
+        
         Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
+        require(sender.weight != 0 || sender.delegateWeight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
-        sender.vote = proposal;
-
-        // If 'proposal' is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
-        proposals[proposal].voteCount += sender.weight;
+        sender.votedTime = block.timestamp;
+        if (opinion) {
+            _ballotParam.observed += sender.weight;
+            _ballotParam.voteCount += (sender.weight + sender.delegateWeight);
+        } else _ballotParam.observed += sender.weight;
+        emit TheVoteWasRegistered(msg.sender,address(this));
     }
 
     /** 
@@ -221,24 +211,28 @@ contract BallotTCN {
      * @return winningProposal_ index of winning proposal in the proposals array
      */
     function voteCountProposal() public view
-            returns (uint256 winningProposal_)
+            returns (uint26 winningProposal_)
     {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
-            }
-        }
+        // uint winningVoteCount = 0;
+        // for (uint p = 0; p < proposals.length; p++) {
+        //     if (proposals[p].voteCount > winningVoteCount) {
+        //         winningVoteCount = proposals[p].voteCount;
+        //         winningProposal_ = p;
+        //     }
+        // }
     }
 
-    /** 
-     * @dev Calls winningProposal() function to get the index of the winner contained in the proposals array and then
-     * @return winnerName_ the name of the winner
-     */
-    function winnerName() public view
-            returns (bytes32 winnerName_)
-    {
-        winnerName_ = proposals[voteCountProposal()].name;
+    function isVotingDeadLine() private returns (bool) {
+        uint48 currentTime = uint48(block.timestamp);
+        if (currentTime <= _deadLine) {
+            return true;
+        } else if (_ballotParam.observed < _votingParams.quorum) {
+            if (currentTime <= (_votingParams.voteStart + _votingParams.voteDuration + _votingParams.etaSeconds)) {
+                _deadLine += _votingParams.etaSeconds;
+                emit etaSecondsIsActivated(address(this), _votingParams.etaSeconds);
+                return true;
+            } 
+        }
+        return false;
     }
 }
